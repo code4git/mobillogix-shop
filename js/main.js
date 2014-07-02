c2b999617755ef03f00f92777904bf5b40f6846e
code4git <ode4gitgmail.com>
2014-07-02 21:48:21 +0300

    slider added

--- js/main.js
+++ js/main.js
@@ -265,8 +265,73 @@
 		
 		return false;
 	});
+	
+	
+    //carousel
+    //rotation speed and timer
+    var speed = 5000;
+    var run = setInterval('rotate()', speed);
+
+    //grab the width and calculate left value
+    var item_width = $('#slides li').outerWidth();
+    var left_value = item_width * (-1);
+
+    //move the last item before first item, just in case user click prev button
+    $('#slides li:first').before($('#slides li:last'));
+
+    //set the default item to the correct position 
+    $('#slides ul').css({'left': left_value});
+    //if user clicked on prev button
+    $('#prev').click(function() {
+        //get the right position            
+        var left_indent = parseInt($('#slides ul').css('left')) + item_width;
+        //slide the item            
+        $('#slides ul').animate({'left': left_indent}, 500, function() {
+            //move the last item and put it as first item                
+            $('#slides li:first').before($('#slides li:last'));
+            //set the default item to correct position
+            $('#slides ul').css({'left': left_value});
+
+        });
+        //cancel the link behavior            
+        return false;
+
+    });
+    //if user clicked on next button
+    $('#next').click(function() {
+
+        //get the right position
+        var left_indent = parseInt($('#slides ul').css('left')) - item_width;
+
+        //slide the item
+        $('#slides ul').animate({'left': left_indent}, 500, function() {
+
+            //move the first item and put it as last item
+            $('#slides li:last').after($('#slides li:first'));
+
+            //set the default item to correct position
+            $('#slides ul').css({'left': left_value});
+
+        });
+
+        //cancel the link behavior
+        return false;
+
+    });
+
+    //if mouse hover, pause the auto rotation, otherwise rotate it
+    $('#slides').hover(
+            function() {
+                clearInterval(run);
+            },
+            function() {
+                run = setInterval('rotate()', speed);
+            }
+    );
 
 });
-
+function rotate() {
+    $('#next').click();
+}
 /*!jquery.customSelect*/
 (function(a){a.fn.extend({customSelect:function(c){if(typeof document.body.style.maxHeight==="undefined"){return this}var e={customClass:"customSelect",mapClass:true,mapStyle:true},c=a.extend(e,c),d=c.customClass,f=function(h,k){var g=h.find(":selected"),j=k.children(":first"),i=g.html()||"&nbsp;";j.html(i);if(g.attr("disabled")){k.addClass(b("DisabledOption"))}else{k.removeClass(b("DisabledOption"))}setTimeout(function(){k.removeClass(b("Open"));a(document).off("mouseup.customSelect")},60)},b=function(g){return d+g};return this.each(function(){var g=a(this),i=a("<span />").addClass(b("Inner")),h=a("<span />");g.after(h.append(i));h.addClass(d);if(c.mapClass){h.addClass(g.attr("class"))}if(c.mapStyle){h.attr("style",g.attr("style"))}g.addClass("hasCustomSelect").on("render.customSelect",function(){f(g,h);g.css("width","");var k=parseInt(g.outerWidth(),10)-(parseInt(h.outerWidth(),10)-parseInt(h.width(),10));h.css({display:"inline-block"});var j=h.outerHeight();if(g.attr("disabled")){h.addClass(b("Disabled"))}else{h.removeClass(b("Disabled"))}i.css({width:k,display:"inline-block"});g.css({"-webkit-appearance":"menulist-button",width:h.outerWidth(),position:"absolute",opacity:0,height:j,fontSize:h.css("font-size")})}).on("change.customSelect",function(){h.addClass(b("Changed"));f(g,h)}).on("keyup.customSelect",function(j){if(!h.hasClass(b("Open"))){g.trigger("blur.customSelect");g.trigger("focus.customSelect")}else{if(j.which==13||j.which==27){f(g,h)}}}).on("mousedown.customSelect",function(){h.removeClass(b("Changed"))}).on("mouseup.customSelect",function(j){if(!h.hasClass(b("Open"))){if(a("."+b("Open")).not(h).length>0&&typeof InstallTrigger!=="undefined"){g.trigger("focus.customSelect")}else{h.addClass(b("Open"));j.stopPropagation();a(document).one("mouseup.customSelect",function(k){if(k.target!=g.get(0)&&a.inArray(k.target,g.find("*").get())<0){g.trigger("blur.customSelect")}else{f(g,h)}})}}}).on("focus.customSelect",function(){h.removeClass(b("Changed")).addClass(b("Focus"))}).on("blur.customSelect",function(){h.removeClass(b("Focus")+" "+b("Open"))}).on("mouseenter.customSelect",function(){h.addClass(b("Hover"))}).on("mouseleave.customSelect",function(){h.removeClass(b("Hover"))}).trigger("render.customSelect")})}})})(jQuery);
